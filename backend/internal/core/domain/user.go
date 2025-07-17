5dcf5ac7fbddadc3256c0d76d3b5bac39c89760f
Nutt <nutt.jan@one.th>
2025-07-17 17:30:53 +0700

    Add error domain & user domain & user port

--- backend/internal/core/domain/user.go
+++ backend/internal/core/domain/user.go
@@ -1,1 +1,13 @@
 package domain
+
+import "time"
+
+type User struct {
+	ID        string    `json:"id"`
+	Username  string    `json:"username"`
+	Password  string    `json:"password"`
+	Email     string    `json:"email"`
+	Role      string    `json:"role"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
+}
--- backend/internal/core/port/user.go
+++ backend/internal/core/port/user.go
@@ -1,1 +1,17 @@
 package port
+
+import (
+	"hr_management/internal/core/domain"
+)
+
+type UserRepository interface {
+	CreateUser(user *domain.User) (*domain.User, error)
+	GetUserByID(id string) (*domain.User, error)
+	UpdateUser(user *domain.User) (*domain.User, error)
+	DeleteUser(id string) error
+}
+
+type UserService interface {
+	Register(user *domain.User) (*domain.User, error)
+	ListUsers([]*domain.User, error)
+}
