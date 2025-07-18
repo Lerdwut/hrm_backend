be6c425a8cef44aedc36675e278cc05934b60df8
Nutt <nutt.jan@one.th>
2025-07-18 11:34:16 +0700

    Add: .gitignore, port and sevice google & Update: main.go

--- backend/cmd/main.go
+++ backend/cmd/main.go
@@ -1,32 +1,49 @@
 package main
 
 import (
-	"github.com/gofiber/fiber/v2"
-	leaveHandler "hr_management/internal/adapter/handler"
-	leaveRepo "hr_management/internal/adapter/storage/mysql/repository"
-	leaveService "hr_management/internal/core/service"
-	"log"
+	"hr_management/internal/adapter/config"
+	mysql "hr_management/internal/adapter/storage/mysql"
+	"log/slog"
+	"os"
 )
 
-func main() {
-	leaveRepo := leaveRepo.NewGormLeaveRepo()
-	leaveService := leaveService.NewLeaveService(leaveRepo)
-	leaveHandler := leaveHandler.NewLeaveHandler(leaveService)
+func Init(config *config.Container) {
 
-	app := fiber.New()
-	api := app.Group("/api")
+	db, err := mysql.NewDatabase(&config.DB)
+	if err != nil {
+		slog.Error("Error initializing database connection", "error", err)
+		os.Exit(1)
+	}
+	defer db.Close()
+
+	err = db.Migrate()
+	if err != nil {
+		slog.Error("Error migrating database", "error", err)
+		os.Exit(1)
+	}
+	slog.Info("Database migrated successfully")
+
+	// Dependency injection
+	//User
+
+	// leaveRepo := leaveRepo.NewGormLeaveRepo()
+	// leaveService := leaveService.NewLeaveService(leaveRepo)
+	// leaveHandler := leaveHandler.NewLeaveHandler(leaveService)
+
+	// app := fiber.New()
+	// api := app.Group("/api")
 	{
-		leaves := api.Group("/leaves")
-		{
-			leaves.Post("/request", leaveHandler.RequestLeave)
-			leaves.Get("/all", leaveHandler.GetAllLeaves)
-			leaves.Put("/:id/approve", leaveHandler.ApprovedLeave)
-			leaves.Put("/:id/reject", leaveHandler.RejectedLeave)
-		}
+		// leaves := api.Group("/leaves")
+		// {
+		// 	leaves.Post("/request", leaveHandler.RequestLeave)
+		// 	leaves.Get("/all", leaveHandler.GetAllLeaves)
+		// 	leaves.Put("/:id/approve", leaveHandler.ApprovedLeave)
+		// 	leaves.Put("/:id/reject", leaveHandler.RejectedLeave)
+		// }
 	}
 
-	log.Println("Starting server on :3000")
-	if err := app.Listen(":3000"); err != nil {
-		log.Fatal("Error starting server:", err)
-	}
+	// log.Println("Starting server on :3000")
+	// if err := app.Listen(":3000"); err != nil {
+	// 	log.Fatal("Error starting server:", err)
+	// }
 }
